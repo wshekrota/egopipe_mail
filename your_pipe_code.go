4b9e1d20fa5792d14a967128cfd0b14e09b79543
Walt Shekrota <penguinacity@gmail.com>
2021-05-12 15:24:12 -0400

    add missing images and correct .go

--- your_pipe_code.go
+++ your_pipe_code.go
@@ -1,8 +1,10 @@
 package main
 
 import "encoding/json"
+
 //import "time"
 //import "log"
+import "regexp"
 
 /*
 
@@ -24,30 +26,31 @@ func yourPipeCode(h map[string]interface{}, c chan *map[string]interface{}) {
 	//
 	p := dotField(h, "log.file.path").(string)
 
-	if p == "/var/log/gitlab/gitaly/current" {
-		// This log is 100% json decode it
-		json.Unmarshal([]byte(h["message"].(string)), &h)
-		// Add a tag to indicate this operation occurred
-		addTags(&h, []string{"DecodedJsonToGitalyFields"})
-		// Create a field using a regex
-		oniguruma("justtime", "time", &h, `(T[0-9:Z]+$)`)
-	}
+	if p == "/var/log/zz.log" {
 
-//	if p == "/var/log/syslog" {
-		// Create multiple fields from one sequential pattern
-//		if !grok(&h, `%{(?P<a>\S+):field1} %{(?P<b>\S+):field2} %{(?P<c>\S+):field3} %{(?P<d>\S+):field4} %{(?P<e>\S+):field5}`) {
-			// If failure add a tag in this doc
-//			addTags(&h, []string{"IfGrokErrorSyslog"})
-//		}
-//	}
-
-	if p == "/var/log/mym.log" {
 		// This log is 100% json decode it
-		json.Unmarshal([]byte(h["message"].(string)), &h)
-		// Add a tag to indicate this operation occurred
-		addTags(&h, []string{"DecodedJsonToMailFields"})
-		// Assign timestamp to actual receive time not log arrival
-		h["@timestamp"] = h["Received"]
+		if json.Unmarshal([]byte(h["message"].(string)), &h) == nil {
+
+			// Add a tag to indicate this operation occurred
+			addTags(&h, []string{"DecodedJsonToMailFields"})
+
+			// Assign timestamp to actual receive time not log arrival
+			h["@timestamp"] = h["Received"]
+
+			fld := h["Text"].(string)
+			r1 := regexp.MustCompile(`(?i)devops`)
+			if r1.Match([]byte(fld)) {
+				h["type_job"] = "Devops"
+			}
+			r2 := regexp.MustCompile(`(?i)developer|programmer`)
+			if r2.Match([]byte(fld)) {
+				h["type_job"] = "Developer"
+			}
+			r3 := regexp.MustCompile(`(?i)analytics|elasticsearch`)
+			if r3.Match([]byte(fld)) {
+				h["type_job"] = "Analytics"
+			}
+		}
 	}
 
 	c <- &h // Although you write code here this line is required
